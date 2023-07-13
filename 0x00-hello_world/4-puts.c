9ddf5b59d1319452e3f53c3d913a64e65713af14
Donwafula <donwafula4@gmail.com>
2023-07-13 07:50:53 -0700

    created 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * puts function to display output
+ * new line
+ * return value 0
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
