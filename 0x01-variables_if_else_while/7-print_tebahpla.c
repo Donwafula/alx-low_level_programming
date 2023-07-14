6d119c6e1361e2c371916cba2ed3343d47da55f3
Donwafula <donwafula4@gmail.com>
2023-07-14 16:13:44 -0700

    7-print_tebahpla.c file

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * decscription: prints lowercase alphabets in reverse*
- * return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int  a = 122;
+	char lowercase_alphabet_reversed = 'z';
 
-	while (a >= 97)
+	while (lowercase_alphabet_reversed >= 'a')
 	{
-		putchar(a);
-		a--;
+		putchar(lowercase_alphabet_reversed);
+		lowercase_alphabet_reversed--;
 	}
 	putchar('\n');
 	return (0);
 }
+
