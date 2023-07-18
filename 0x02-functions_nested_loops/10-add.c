6452310295a2c8803e3046718f09e8ee2be9de5e
Donwafula <donwafula4@gmail.com>
2023-07-18 14:22:50 -0700

    task 11 file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @i: int type number
+ * @j: int type number
+ * Return:0
+ */
+
+
+int add(int i, int j)
+{
+return (i + j);
+}
