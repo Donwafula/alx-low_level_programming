ae2c3106cc270e7dfd07f3b0d45c1d5a26aca566
Donwafula <donwafula4@gmail.com>
2023-07-20 07:11:06 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description: prints 1 to 100, fizz, buzz, fizzbuz
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % (3 * 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", a);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
