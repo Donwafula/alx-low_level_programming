b429a959011f1bfbb9ee68573366ed6b981e7b5e
Donwafula <donwafula4@gmail.com>
2023-07-24 10:29:03 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as and updates the valut to 98
+ * @*n: integer to be reset
+ * @n: pointer
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05 project: arrays pointers, strings
