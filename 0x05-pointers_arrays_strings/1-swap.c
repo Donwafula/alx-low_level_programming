9820681025560fb536084f2c55eff51ef701783e
Donwafula <donwafula4@gmail.com>
2023-07-24 11:00:26 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @*a: pointer to first integer
+ * @*b: pointer to second intger
+ * @a: first integer
+ * @b: second integer
+ * return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
