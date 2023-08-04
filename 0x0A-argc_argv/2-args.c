325950e23edcc6c3677b44ab7dfff89c39c0a711
Donwafula <donwafula4@gmail.com>
2023-08-04 15:26:01 -0700

    Project task files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Retun: Always 0 (Sucess)
+ */
+
+int main(int argc, char **argv)
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
