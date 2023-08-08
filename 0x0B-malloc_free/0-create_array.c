bff18bb6c12edbb88c3dcf096db1ac520160fc99
Donwafula <donwafula4@gmail.com>
2023-08-08 13:47:00 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,11 +14,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int a;
 	char *s;
 
-	s = (char *) malloc(size * sizeof(char));
+	s = malloc(size * sizeof(char));
 	if (s == 0 || s == NULL)
-	{
 		return (NULL);
-	}
 	for (a = 0; a < size; a++)
 	{
 		s[a] = c;
