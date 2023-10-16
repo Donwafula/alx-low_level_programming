05d50eee35d764ae4f9ab30a55ab44e423d08b2f
Donwafula <donwafula4@gmail.com>
2023-10-16 12:39:53 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/codes.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - sums
+ * @a: num1
+ * @b: num2
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts
+ * @a: num1
+ * @b: num2
+ * Return: dif
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies
+ * @a: num1
+ * @b: num2
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - division
+ * @a: num1
+ * @b: num2
+ * Return: div
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - moduls
+ * @a: num1
+ * @b: num2
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
