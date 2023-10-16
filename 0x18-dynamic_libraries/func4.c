8ff219d7c8229035066db140764cf03f8f7d6749
Donwafula <donwafula4@gmail.com>
2023-10-16 22:21:00 +0300

    Corrections

--- 0x18-dynamic_libraries/func4.c
+++ 0x18-dynamic_libraries/func4.c
@@ -96,3 +96,42 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return (0);
 }
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String
+ * @needle: Substring
+ * Return: The substring in the string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int letter, needle_size, i;
+	char *pos;
+
+	letter = 0;
+	needle_size = _strlen(needle);
+	if (needle_size == 0)
+		return (haystack);
+
+	for (; *haystack != 0; haystack++)
+	{
+		if (*haystack == needle[0])
+		{
+			pos = haystack;
+			for (i = 0; needle[i] != 0 && *haystack != 0; i++, haystack++)
+			{
+				if (*haystack != needle[i])
+				{
+					letter = 0;
+					break;
+				}
+				letter++;
+			}
+		}
+	}
+
+	if (letter == needle_size)
+		return (pos);
+
+	return (0);
+}
