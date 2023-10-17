e71b6b55685ff604087bdf320079b464febe442e
Donwafula <donwafula4@gmail.com>
2023-10-17 09:53:11 +0300

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcc -shared -o libjackpot.so -fPIC -Wl,--no-as-needed -ldl jackpot.c
-LD_PRELOAD=./libjackpot.so
+echo -e '#define _GNU_SOURCE\n#include <stdio.h>\n#include <stdlib.h>\n#include <dlfcn.h>\n\nint rand(void) { return 9; }' > libjackpot.c && gcc -shared -o libjackpot.so -fPIC -Wl,--no-as-needed -ldl libjackpot.c
